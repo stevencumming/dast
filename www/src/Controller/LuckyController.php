dee8366bad88139ab3c888a5845aa1b37c155b67
Parker Young <68844399+ParkerYoungOFCL@users.noreply.github.com>
2023-08-01 00:29:22 +1000

    Creating test page
    Test page to verify functionality of server and Symfony project functionality. Pulled from Symfony Docs.

---
+++ www/src/Controller/LuckyController.php
@@ -0,0 +1,18 @@
+// src/Controller/LuckyController.php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LuckyController
+{
+    #[Route('/test')]
+    public function number(): Response
+    {
+        $number = random_int(0, 100);
+
+        return new Response(
+            '<html><body>Lucky number: '.$number.'</body></html>'
+        );
+    }
+}
