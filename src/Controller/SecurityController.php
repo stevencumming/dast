a480913026811a0c0dfeac075ebacb40b0c3823a
Parker Young <pmyoung@outlook.com.au>
2023-08-28 22:53:16 +1000

    Security/login structure
    Using dashboard as main controller. Security authorisation set up. Can implement a public controller in future if we want public facing pages.

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -9,12 +9,13 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
-    #[Route(path: '/login', name: 'app_login')]
+    #[Route(path: '/', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
+        // checks if user is logged in and redirects to dashboard
+        if ($this->getUser()) {
+            return $this->redirectToRoute('dashboard');
+        }
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
