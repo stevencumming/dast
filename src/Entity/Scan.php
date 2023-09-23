a737834ec95f67ecfebf106192ccdb07b55173d0
stevencumming <101092147@student.swin.edu.au>
2023-09-24 07:57:55 +1000

    implementing new db structure and entities

--- src/Entity/Scan.php
+++ src/Entity/Scan.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\ScanRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
@@ -32,6 +34,18 @@ class Scan
 
     #[ORM\Column(type: Types::DATETIME_MUTABLE)]
     private ?\DateTimeInterface $time_completed = null;
+
+    #[ORM\OneToMany(mappedBy: 'scan_id', targetEntity: Tool::class)]
+    private Collection $tools;
+
+    #[ORM\OneToMany(mappedBy: 'scan_id', targetEntity: Vulnerability::class)]
+    private Collection $vulnerabilities;
+
+    public function __construct()
+    {
+        $this->tools = new ArrayCollection();
+        $this->vulnerabilities = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -106,6 +120,66 @@ class Scan
     public function setTimeCompleted(\DateTimeInterface $time_completed): static
     {
         $this->time_completed = $time_completed;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Tool>
+     */
+    public function getTools(): Collection
+    {
+        return $this->tools;
+    }
+
+    public function addTool(Tool $tool): static
+    {
+        if (!$this->tools->contains($tool)) {
+            $this->tools->add($tool);
+            $tool->setScanId($this);
+        }
+
+        return $this;
+    }
+
+    public function removeTool(Tool $tool): static
+    {
+        if ($this->tools->removeElement($tool)) {
+            // set the owning side to null (unless already changed)
+            if ($tool->getScanId() === $this) {
+                $tool->setScanId(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Vulnerability>
+     */
+    public function getVulnerabilities(): Collection
+    {
+        return $this->vulnerabilities;
+    }
+
+    public function addVulnerability(Vulnerability $vulnerability): static
+    {
+        if (!$this->vulnerabilities->contains($vulnerability)) {
+            $this->vulnerabilities->add($vulnerability);
+            $vulnerability->setScanId($this);
+        }
+
+        return $this;
+    }
+
+    public function removeVulnerability(Vulnerability $vulnerability): static
+    {
+        if ($this->vulnerabilities->removeElement($vulnerability)) {
+            // set the owning side to null (unless already changed)
+            if ($vulnerability->getScanId() === $this) {
+                $vulnerability->setScanId(null);
+            }
+        }
 
         return $this;
     }
--- src/Scan/ScanProcessor.php
+++ src/Scan/ScanProcessor.php
@@ -7,9 +7,21 @@ use Symfony\Component\Clock\ClockInterface;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Bundle\SecurityBundle\Security;
-use App\Entity\Process;
+use App\Entity\Tool;
+use App\Entity\Vulnerability;
 use App\Entity\Scan;
+
 use App\Service\DdosService;
+
+// Tools
+use App\Service\TOOL_DummyTool;
+
+
+// Vulnerabilities
+use App\Service\VULN_DummyVulnerability;
+use Doctrine\ORM\Query\Expr;
+
+// ========================================================================
 
 // currently rigged up for one process as of moment
 class ScanProcessor
@@ -21,9 +33,32 @@ class ScanProcessor
 
         private Scan $scan,
 
-        private Process $ddosProcess,
+        // Tools
+        // private Process $ddosProcess,        (replaced by Tool)
+        
+        private Tool $Tdummy,
+        private Tool $Tsomething,
+        private Tool $Tsomethingelse,
+
+        // Vulnerabilities
+        private Vulnerability $Vdummy,
+
+
+
     ) {
     }
+
+
+    /*
+        Order of operations:
+        1.  Load scan (loadScan)
+        2.  Mark that the scan has commenced (  $this->scan->scanCommenced();  )
+        3.  Process all of the tools against the target of this scan (processTools)
+        4.  Process each vulnerability, analysing the results of executed tools (processVulnerabilities)
+        5.  Generate the PDF and email (compileReport)
+        6.  Mark that the scan has completed (  $this->scan->scanComplete();  )
+
+    */
 
     public function loadScan(string $target) {
         // load the entity manager for initial persist
@@ -40,28 +75,6 @@ class ScanProcessor
         $em->flush(); 
     }
 
-    public function processCompute() {
-        // mark the scan commenced
-        $this->scan->scanCommenced();
-
-        // ddos process, repeat this for all scan processes
-        $this->ddosProcess->setStatus('waiting');
-        $ddosService = new DdosService($ddosProcess);
-        $ddosResults = $ddosService->cdnCheck();
-        if (!$ddosResults == null) {
-            $this->ddosProcess->setResults($ddosService->cdnCheck());
-            $this->ddosProcess->setStatus('completed');
-            $em->persist($ddosProcess);
-            $em->flush();    
-        } else {
-            $this->ddosProcess->setStatus('error');  
-        }
-        $em->persist($ddosProcess);
-        $em->flush();
-
-        // mark scan complete
-        $this->scan->scanComplete();
-    }
 
     public function scanCommenced() {
         // mark scan commencement time and persist
@@ -71,6 +84,128 @@ class ScanProcessor
         $em->flush(); 
     }
 
+
+    public function processTools() {
+        // The first process to be called after a new scan is loaded.
+        // Go through all tools to be used and execute them.
+
+
+        // =============== TOOL ===============
+        // Tool Name:       Dummy Tool
+        // Responsible:     AA
+
+        // Set the scan ID for the tool
+        $this->Tdummy->setScanId($this->scan);
+
+        // Set the name of the tool
+        $this->Tdummy->setName("nslookup");
+
+        // Declare the process (service)
+        $TdummyProcess = new TOOL_DummyTool($this->Tdummy);
+
+        // Start the tool process execution
+        $TdummyProcess->Execute();
+
+        // Error handling is done in the tool service... persist the output to database
+        $this->Tdummy->setResults($TdummyProcess->Output());
+        $em->persist($Tdummy);
+        $em->flush();  
+
+        // DONE
+        // Move on to the next tool...
+    
+        // ...
+        // ...
+
+        // =============== TOOL ===============
+        // Tool Name:       Another Tool
+        // Responsible:     AA
+        $this->Tanother->setScanId($this->scan);
+        $this->Tanother->setName("nslookup");
+        $TanotherProcess = new TOOL_DummyTool($this->Tanother);
+        $TanotherProcess->Execute();
+        $this->Tanother->setResults($TanotherProcess->Output());
+        $em->persist($Tanother);
+        $em->flush(); 
+
+        // ...
+        // ...
+
+        // =============== TOOL ===============
+        // Tool Name:       The next tool
+        // Responsible:     AA
+
+
+    }
+
+
+    public function processVulnerabilities() {
+        // Go through each of the vulnerabilities and analyse output of the relevant tool(s)
+        
+        // =============== VULNERABILITY ===============
+        // Vulnerability:       
+        // Responsible: 
+        
+        // Set the scan ID for the vulnerability
+        $this->Vdummy->setScanId($this->scan);
+
+        // Set the name of the tool
+        $this->Vdummy->setName("Some vulnerability");
+
+        // Analyse
+        $VdummyProcess = new VULN_DummyVulnerability($this->Vdummy);
+
+        // Persist Results
+        $this->Vdummy->setSeverity($VdummyProcess->getSeverity());
+        $this->Vdummy->setHtml($VdummyProcess->getHTML());
+
+        $em->persist($Vdummy);
+        $em->flush();  
+
+        // DONE
+        // Move on to the next vulnerability...
+    
+        // ...
+        // ...
+
+        // =============== VULNERABILITY ===============
+        // Vulnerability:       
+        // Responsible: 
+        $this->Vanother->setScanId($this->scan);
+        $this->Vanother->setName("Some vulnerability");
+        $VanotherProcess = new VULN_AnotherVulnerability($this->Vanother);
+        $this->Vanother->setSeverity($Vanother->getSeverity());
+        $this->Vanother->setHtml($Vanother->getHTML());
+        $em->persist($Vanother);
+        $em->flush();  
+
+        // ...
+        // ...
+
+        // =============== VULNERABILITY ===============
+        // Vulnerability:       
+        // Responsible: 
+
+    }
+
+
+    public function compileReport() {
+        // Compile the report using the vulnerability data from analysis above.
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+    
+
     public function scanComplete() {
         // mark scan conclusion time and persist
         $em = $this->entityManager;
@@ -78,5 +213,46 @@ class ScanProcessor
         $em->persist($this->scan);
         $em->flush(); 
     }
+
+
+
+
+
+
+
+
+
+
+
+    // public function processCompute() {
+    //     // mark the scan commenced
+    //     $this->scan->scanCommenced();
+        
+
+    //     // ddos process, repeat this for all scan processes
+    //     $this->ddosProcess->setStatus('waiting');
+    //     $ddosService = new DdosService($ddosProcess);
+    //     $ddosResults = $ddosService->cdnCheck();
+    //     if (!$ddosResults == null) {
+    //         $this->ddosProcess->setResults($ddosService->cdnCheck());
+    //         $this->ddosProcess->setStatus('completed');
+    //         $em->persist($ddosProcess);
+    //         $em->flush();    
+    //     } else {
+    //         $this->ddosProcess->setStatus('error');  
+    //     }
+    //     $em->persist($ddosProcess);
+    //     $em->flush();
+
+    //     // mark scan complete
+    //     $this->scan->scanComplete();
+    // }
+
+
+
+
+
+
+    
 
 }
