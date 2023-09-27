22faa058f9d29864b8b9f4d62d18520d8ef0ca6a
gorissenm <110945627+gorissenm@users.noreply.github.com>
2023-09-27 17:33:55 +1000

    Updated regex + rework of functionality

--- src/Service/TOOL_Nmap.php
+++ src/Service/TOOL_Nmap.php
@@ -41,11 +41,8 @@ class TOOL_Nmap {
         // Example: file listing (generic command plus argument)
         //$process = new Process(['ls', '-lsa']);
 
-        // Example: nslookup of Swinburne
-        //$this->process = new Process(['nslookup', 'swin.edu.au']);
-
         
-        $this->process = new Process(['nmap', '-sC -sV --script vulners', $tool->getScanId()->getTarget()]); // idk if getScanId actually returns the scan object...
+        $this->process = new Process(['nmap', '-T4', '-A', '--script vulners', $tool->getScanId()->getTarget()]); // idk if getScanId actually returns the scan object...
 
         // ... where there are multiple processes, name them '$process_nslookup' and '$process_namp' for example.
 
@@ -88,10 +85,25 @@ class TOOL_Nmap {
 
         //not sure if we want to use multiple patterns like this, will have to look into the preg_match_all function more deeply to determine
 
-        $patternPorts = 'XX/tcp';
-        $patterCves = 'TODO';
-        $patternHostInfo = 'TODO';
-        preg_match_all($pattern, $CLI, $matches, PREG_SET_ORDER, 0);
+        // regex looking for 1-5 digits followed by / followed by tcp or udp
+        $patternPorts = '\d{1,5}[/](tcp|udp)';
+        // regex looking for CVE followed by -, then 4 numbers, another - and then 4 to 5 numbers then white space
+        $patternCves = 'CVE-\d{4}-\d{4,5}\s';
+        
+        $patternHostOS = 'TODO';
+        
+        $patternServerVersion = 'TODO';
+
+        // TODO need to figure out how this needs to be implemented, whether or not there should be multiple preg_match_all or not
+        
+        preg_match_all($patternPorts, $CLI, $ports, PREG_SET_ORDER, 0);
+        preg_match_all($patternCves, $CLI, $cves, PREG_SET_ORDER, 0);
+        preg_match_all($patternHostOS, $CLI, $hostInfo, PREG_SET_ORDER, 0);
+        preg_match_all($patternServerVersion, $CLI, $hostInfo, PREG_SET_ORDER, 0);
+
+
+        // if multiple preg_match statements can be used I don't think we will need the below, since those statements populate their respective arrays if they are taken in as arguments 
+        // instead of using a generic "matches" array and then adding certain entries to the correct arrays
 
         // normally, I'm not a fan of ternary or single-line if statements.. but it makes sense here.
         //if (isset($matches[0])) array_push($this->names, $matches[0]);
@@ -100,8 +112,36 @@ class TOOL_Nmap {
         //if (isset($matches[3])) array_push($this->addresses, $matches[1]);
     }
 
+    // I have added a few getters here that are for specific arrays like ports and cves etc, it might be helpful to have for when the vulnerabilities need to be called they can just check relevant output 
+    
+    public function FuckoFF() {
+
+    }
+
+    public function GetPorts() {
+        
+        return this->ports;
+
+    }
+
+    public function GetCVEs() {
+    
+        return $this->cves;
+
+    }
+
+    public function GetHostInfo() {
+
+        return $this->hostInfo;
+
+    }
+
+    
 
     public function Output() {
+
+        
+
         // Persist this whole object as JSON
         // TODO
         // return json_encode($this);
