121b74247d85621741e705a5c30e0fec63a067e6
BLueberryHouse <cumming.lachlan@gmail.com>
2023-10-06 12:24:34 +0000

    XSStrike tool refractor

--- processor/tools/TOOL_XSStrike.php
+++ processor/tools/TOOL_XSStrike.php
@@ -5,7 +5,7 @@ class TOOL_XSRFProbe extends TOOL {
     /*
         Tool Name:              XSStrike
         Responsible:            LC
-        OpenProject Phase #:    XXX
+        OpenProject Phase #:    428
 
         Summary:
             ... quick summary of the tool's purpose. (Even if it's all written in PHP / Symfony, still describe it breifly)
@@ -22,18 +22,24 @@ class TOOL_XSRFProbe extends TOOL {
 
     public function Execute() {
         // Run the process(es)
-        $this->vulnTypes = [];
+        $this->component = [];
+        $this->cves = [];
 
-        $command = 'python xsstrike.py -u "http://example.com/page.php" --crawl';
+        $command = 'python xsstrike.py -u "http://127.0.0.1/mutillidae/" --crawl';
         exec($command, $CLI);
         
-        $patternTypes = '#Possible Vulnerability Type:#';
+        $patternComponents = '#Vulnerable component:.*+#';
+        $patternCves = '#CVE:.*+#';
 
         foreach($CLI as $line){
 
-            preg_match_all($patternTypes, $line, $result, PREG_SET_ORDER, 0);
+            preg_match_all($patternComponents, $line, $result, PREG_SET_ORDER, 0);
             if(isset($result[0])) {
-                // it actually might be better to push the whole line to the array since the match string doesn't really give us anything useful
+                array_push($this->vulnTypes, $line);
+            }
+
+            preg_match_all($patternCves, $line, $result, PREG_SET_ORDER, 0);
+            if(isset($result[0])) {
                 array_push($this->vulnTypes, $line);
             }
 
@@ -45,9 +51,15 @@ class TOOL_XSRFProbe extends TOOL {
     // no need for setter since only the tool class can set its arrays
 
 
-    public function getVulnTypes() {
+    public function getComponents() {
 
-        return $this->vulnTypes;
+        return $this->components;
+
+    }
+
+    public function getCves() {
+
+        return $this->cves;
 
     }
 
